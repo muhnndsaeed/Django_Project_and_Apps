4eb16db841f20875fe30a1e03264703bb2f8e991
mhnd <mhnd@hno.com>
2022-05-23 11:11:04 +0300

    added file

--- Movies/MoviesListApp/views.py
+++ Movies/MoviesListApp/views.py
@@ -3,6 +3,6 @@ from django.http import render
 
 
 def Movie_Name(request):
-page = request.GET.get("page")
-    return render(request,"base.html",{'page':page})
+
+    return render(request,"base.html")
 
